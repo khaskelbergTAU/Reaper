a67bb3907e2703b0fab328b5ff414bdce13e590b
yuvalK <yuval@khaskelberg.org>
2024-04-26 07:23:20 +0300

    config values were incorrect, causing config and code to not match up. fixed.

--- common/src/main/java/earth/terrarium/reaper/common/config/ReaperConfig.java
+++ common/src/main/java/earth/terrarium/reaper/common/config/ReaperConfig.java
@@ -17,39 +17,39 @@ public final class ReaperConfig {
     @Comment(value = "The maximum amount of energy the soul beacon can store.")
     public static int soulBeaconEnergyCap = 1000000;
 
-    @ConfigEntry(id = "soul_beacon_energy_prod", type = EntryType.INTEGER, translation = "config.reaper.soul_beacon_energy_prod")
-    @Comment(value = "The amount of energy the soul beacon produces per tick.")
+    @ConfigEntry(id = "generator_energy_cap", type = EntryType.INTEGER, translation = "config.reaper.generator_energy_cap")
+    @Comment(value = "The maximum amount of energy generator can store.")
     public static int genEnergyCap = 1000000;
 
-    @ConfigEntry(id = "soul_beacon_max_range", type = EntryType.INTEGER, translation = "config.reaper.soul_beacon_max_range")
-    @Comment(value = "The maximum range the soul beacon can affect mobs.")
+    @ConfigEntry(id = "generator_energy_prod", type = EntryType.INTEGER, translation = "config.reaper.generator_energy_prod")
+    @Comment(value = "The maximum range the generator can affect mobs.")
     public static int genEnergyProd = 80;
 
-    @ConfigEntry(id = "soul_beacon_energy_prod_with_rune", type = EntryType.INTEGER, translation = "config.reaper.soul_beacon_energy_prod_with_rune")
-    @Comment(value = "The amount of energy the soul beacon produces per tick with the rune.")
+    @ConfigEntry(id = "generator_energy_prod_with_rune", type = EntryType.INTEGER, translation = "config.reaper.generator_energy_prod_with_rune")
+    @Comment(value = "The amount of energy the generator produces per tick with the rune.")
     public static int genEnergyProdWithRune = 160;
 
-    @ConfigEntry(id = "soul_beacon_max_range", type = EntryType.INTEGER, translation = "config.reaper.soul_beacon_max_range")
-    @Comment(value = "The maximum range the soul beacon can affect mobs.")
+    @ConfigEntry(id = "generator_max_range", type = EntryType.INTEGER, translation = "config.reaper.generator_max_range")
+    @Comment(value = "The maximum range the generator can affect mobs.")
     public static int genMaxRange = 5;
 
-    @ConfigEntry(id = "soul_beacon_max_range_with_rune", type = EntryType.INTEGER, translation = "config.reaper.soul_beacon_max_range_with_rune")
-    @Comment(value = "The maximum range the soul beacon can affect mobs with the rune.")
+    @ConfigEntry(id = "generator_max_range_with_rune", type = EntryType.INTEGER, translation = "config.reaper.generator_max_range_with_rune")
+    @Comment(value = "The maximum range the generator can affect mobs with the rune.")
     public static int genMaxRangeWithRune = 8;
 
-    @ConfigEntry(id = "soul_beacon_damage", type = EntryType.INTEGER, translation = "config.reaper.soul_beacon_damage")
-    @Comment(value = "The amount of damage the soul beacon deals to mobs. If this value is -1, the mob will die instantly.")
+    @ConfigEntry(id = "generator_damage", type = EntryType.INTEGER, translation = "config.reaper.generator_damage")
+    @Comment(value = "The amount of damage the generator deals to mobs. If this value is -1, the mob will die instantly.")
     public static int genDamage = 5;
 
-    @ConfigEntry(id = "soul_beacon_damage_with_rune", type = EntryType.INTEGER, translation = "config.reaper.soul_beacon_damage_with_rune")
-    @Comment(value = "The amount of damage the soul beacon deals to mobs with the rune. If this value is -1, the mob will die instantly.")
+    @ConfigEntry(id = "generator_damage_with_rune", type = EntryType.INTEGER, translation = "config.reaper.generator_damage_with_rune")
+    @Comment(value = "The amount of damage the generator deals to mobs with the rune. If this value is -1, the mob will die instantly.")
     public static int genDamageWithRune = -1;
 
-    @ConfigEntry(id = "soul_beacon_cooldown", type = EntryType.INTEGER, translation = "config.reaper.soul_beacon_cooldown")
-    @Comment(value = "The cooldown of the soul beacon.")
+    @ConfigEntry(id = "generator_cooldown", type = EntryType.INTEGER, translation = "config.reaper.generator_cooldown")
+    @Comment(value = "The cooldown of the generator.")
     public static int genCooldown = 120;
 
-    @ConfigEntry(id = "soul_beacon_cooldown_with_rune", type = EntryType.INTEGER, translation = "config.reaper.soul_beacon_cooldown_with_rune")
-    @Comment(value = "The cooldown of the soul beacon with the rune.")
+    @ConfigEntry(id = "generator_cooldown_with_rune", type = EntryType.INTEGER, translation = "config.reaper.generator_cooldown_with_rune")
+    @Comment(value = "The cooldown of the generator with the rune.")
     public static int genCooldownWithRune = 60;
 }
